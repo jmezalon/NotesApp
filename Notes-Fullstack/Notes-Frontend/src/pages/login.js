d10417ae5803b9b922525300b29908f88bac65ed
Ben Jackson <benjacksonSE@gmail.com>
2021-12-06 15:29:57 -0500

    Login css and Routing done

--- Notes-Fullstack/Notes-Frontend/src/pages/login.js
+++ Notes-Fullstack/Notes-Frontend/src/pages/login.js
@@ -20,12 +20,12 @@ const Login = () => {
           <div className="centerText">OR</div>
         </div>
         <div className="right-side">
-          {/* <h2 className="form-header">New User</h2>
+          <h2 className="form-header">New User</h2>
           <input type="text" placeholder="Username" />
           <input type="text" placeholder="Password" />
           <input type="text" placeholder="Email" />
-          <button classname="submit">Sign Up</button>
-          <h2 className="form-header">Already a user</h2> */}
+          <button className="submit">Sign Up</button>
+          <h2 className="form-header">Already a user</h2>
           <input type="text" placeholder="Username" />
           <input type="text" placeholder="Password" />
           <button className="submit">Login</button>
